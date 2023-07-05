2f6191733f54db8262164b1a5c8ed0d3254bd142
viktoriia-yash <yashkina.vic@gmail.com>
2023-07-05 18:57:42 +0300

    HTML code for the Home page
    Added input to enter player's name, chose quiz category, difficulty, and button to start the game

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,39 @@
+import { Button, MenuItem, TextField } from "@mui/material";
 import React from "react";
+import Categories from "../../data/Categories";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="content home-body">
+      <div className="home-container">
+        <span style={{ fontSize: 30 }}>Quiz Settings</span>
+        <div className="settings">
+          <TextField label="Enter Your Name" variant="outlined" />
+          <TextField select label="Select Category" variant="outlined">
+            {Categories.map((cat) => (
+              <MenuItem key={cat.category} value={cat.value}>
+                {cat.category}
+              </MenuItem>
+            ))}
+          </TextField>
+
+          <TextField select label="Select Difficulty" variant="outlined">
+            <MenuItem key="Easy" value="easy">
+              Easy
+            </MenuItem>
+            <MenuItem key="Medium" value="medium">
+              Medium
+            </MenuItem>
+            <MenuItem key="Hard" value="hard">
+              Hard
+            </MenuItem>
+          </TextField>
+
+          <Button>Start Quiz</Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
