ee8108a53c345cb35eea3fde3cd6dd7ec2e5995f
viktoriia-yash <yashkina.vic@gmail.com>
2023-07-05 16:52:06 +0300

    New project + new components + built project structure
    Deleted some unnecessary  elements

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./component/Header/Header";
 import Footer from "./component/Footer/Footer";
+import Home from "./pages/Home/Home";
+import Quiz from "./pages/Quiz/Quiz";
+import Result from "./pages/Result/Result";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/quiz" Component={Quiz} />
+          <Route path="/result" Component={Result} />
+        </Routes>
       </div>
       <Footer />
     </BrowserRouter>
--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -5,6 +5,7 @@ const Header = () => {
   return (
     <div>
       <Link to="/">Quiz App</Link>
+      
     </div>
   );
 };
