bf54f98abb4173d2870af801c1277e4bd2d0ebe3
viktoriia-yash <yashkina.vic@gmail.com>
2023-07-06 15:05:20 +0300

    Finished page with questions
    All needed code from HTML to conditions. I was about to finish this project two days ago, but unfortunately, I got sick, and now we are here. Sorry

---
+++ src/component/Question/Question.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router";
+
+const Question = ({
+  currentQuestion,
+  setCurrentQuestion,
+  questions,
+  options,
+  correct,
+  score,
+  setScore,
+  setQuestions,
+}) => {
+  const [selectedQ, setSelectedQ] = useState();
+  const [error, setError] = useState(false);
+
+  const handleSelect = (i) => {
+    if (selectedQ === i && selectedQ === correct) {
+      return "select";
+    } else if (selectedQ === i && selectedQ !== correct) {
+      return "wrong";
+    } else if (i === correct) {
+      return "select";
+    }
+  };
+
+  const handleCheck = (i) => {
+    setSelectedQ(i);
+    if (i === correct) setScore(score + 1);
+    setError(false);
+  };
+
+  const navigate = useNavigate();
+
+  const handleNext = () => {
+    if (currentQuestion > 8) {
+      navigate("/result");
+    } else if (selectedQ) {
+      setCurrentQuestion(currentQuestion + 1);
+      setSelectedQ();
+    } else {
+      setError("Please select an option first");
+    }
+  };
+
+  const handleQuit = () => {};
+
+  return (
+    <div>
+      <h3>Question {currentQuestion + 1}</h3>
+      <div>
+        <h3>{questions[currentQuestion].question}</h3>
+        <div>
+          {error && <ErrorMessage>{error}</ErrorMessage>}
+          {options &&
+            options.map((i) => (
+              <button
+                onClick={() => handleCheck(i)}
+                key={i}
+                disabled={selectedQ}
+                className={`singleOption ${selectedQ && handleSelect(i)}`}
+              >
+                {i}
+              </button>
+            ))}
+        </div>
+        <div className="control-btns">
+          <Button variant="contained" href="/" onClick={handleQuit}>
+            Quit Quiz
+          </Button>
+          <Button variant="contained" onClick={handleNext}>
+            Next Question
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Question;
--- src/pages/Quiz/Quiz.js
+++ src/pages/Quiz/Quiz.js
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { CircularProgress } from "@mui/material";
+import Question from "../../component/Question/Question";
 
 const Quiz = ({ name, score, questions, setQuestions, setScore }) => {
-  return <div>Quiz</div>;
+  const [options, setOptions] = useState();
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  useEffect(() => {
+    console.log(questions);
+    setOptions(
+      questions &&
+        handleShuffle([
+          questions[currentQuestion]?.correct_answer,
+          ...questions[currentQuestion]?.incorrect_answers,
+        ])
+    );
+  }, [questions, currentQuestion]);
+
+  console.log(options);
+
+  const handleShuffle = (optioncs) => {
+    return optioncs.sort(() => Math.random() - 0.5);
+  };
+
+  return (
+    <div>
+      <span>Welcome, {name}</span>
+
+      {questions ? (
+        <>
+          <div>
+            <span>{questions[currentQuestion].category}</span>
+            <span>Score: {score}</span>
+          </div>
+          <Question
+            currentQuestion={currentQuestion}
+            setCurrentQuestion={setCurrentQuestion}
+            questions={questions}
+            options={options}
+            correct={questions[currentQuestion]?.correct_answer}
+            score={score}
+            setScore={setScore}
+            setQuestions={setQuestions}
+          />
+        </>
+      ) : (
+        <CircularProgress />
+      )}
+    </div>
+  );
 };
 
 export default Quiz;
